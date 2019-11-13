1bd26ea05a54c95da4fdc3ccb773e0047c7c9153
LuaGanrqunx <aqunxgm@gmail.com>
2019-11-13 12:20:42 +0800

    finish pre class test

--- hello_toast/src/main/java/com/example/hello_toast/MainActivity.java
+++ hello_toast/src/main/java/com/example/hello_toast/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.hello_toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView)findViewById(R.id.tvShowCount);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, "Hello, Toast", Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+
+    private int mCount = 0;
+    private TextView mShowCount;
+    public void countUp(View view) {
+        mCount++;
+        if(mShowCount != null){
+            mShowCount.setText(Integer.toString(mCount));
+
+        }
     }
 }
